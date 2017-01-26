c5f78e029a0a6de0c184c6dca19308cc6adcbbd8
hazimhanif <hazimhanif0@gmail.com>
2017-01-26 21:14:15 +0800

    16 features!

--- playstore_features_extractor/feature_creation.R
+++ playstore_features_extractor/feature_creation.R
@@ -1,3 +1,7 @@
+## Importing library
+library(stringdist)
+
+
 ## Source the functions file
 source("feature_creation_functions.R")
 
@@ -13,12 +17,12 @@ dataset<-dataset[,1:13]
 
 
 ##Remove all punctuations
-print("Removing all punctuations..")
-for(x in seq(1,nrow(dataset))){
-  dataset[x,"revTitle"]<- gsub(dataset[x,"revTitle"],pattern = "[[:punct:]]",replacement = "")
-  dataset[x,"revText"]<- gsub(dataset[x,"revText"],pattern = "[[:punct:]]",replacement = "")
+#print("Removing all punctuations..")
+#for(x in seq(1,nrow(dataset))){
+dataset[x,"revTitle"]<- gsub(dataset[x,"revTitle"],pattern = "[[:punct:]]",replacement = "")
+dataset[x,"revText"]<- gsub(dataset[x,"revText"],pattern = "[[:punct:]]",replacement = "")
 }
-
+#
 
 ##Converting date to real date type
 print("Converting date data to real date type.")
@@ -45,13 +49,22 @@ for(x in seq(1,nrow(dataset))){
 dataset$revDate<-as.Date(dataset$revDate,"%d-%m-%Y")
 
 
-##Features Extraction
+##Features Extraction: Total 16
+print("Extracting features.")
 app_price<-obs$appPrice
 app_score<-obs$appScore
 rev_title_len<-len(obs$revTitle)
 rev_body_len<-len(obs$revText)
 rev_pos_ascend<-func_rev_pos_ascend(obs)
 rev_pos_descend<-func_rev_pos_descend(obs)
-first_rev<-func_first_rev
-only_rev<-func_only_rev
+first_rev<-func_first_rev(obs)
+only_rev<-func_only_rev(obs)
+avg_cosine_similarity_title<-func_avg_cosine_similarity_title(obs)
+avg_cosine_similarity_text<-func_avg_cosine_similarity_text(obs)
+avg_levenshtein_dist_title<-func_avg_levenshtein_dist_title(obs)
+avg_levenshtein_dist_text<-func_avg_levenshtein_dist_text(obs)
+brand_names_in_title<-func_brand_names_in_title(obs)
+brand_names_in_text<-func_brand_names_in_text(obs)
+num_title_ratio<-length(as.numeric(unlist(strsplit(gsub("[^0-9]", "", unlist(obs$revTitle)), ""))))/length(obs$revTitle)
+num_text_ratio<-length(as.numeric(unlist(strsplit(gsub("[^0-9]", "", unlist(obs$revText)), ""))))/length(obs$revText)
 
--- playstore_features_extractor/feature_creation_functions.R
+++ playstore_features_extractor/feature_creation_functions.R
@@ -23,6 +23,57 @@ func_only_rev<-function(obs){
   {1}else{0}
 }
 
+func_avg_cosine_similarity_title<-function(obs){
+  temp<-dataset[dataset$appId==obs$appId,]
+  tempList<-c()
+  for(x in seq(1,nrow(temp))){
+    tempList<-c(tempList,stringdist(obs$revTitle,dataset[x,"revTitle"], method ="cosine"))
+  }
+  mean(tempList)
+}
+
+func_avg_cosine_similarity_text<-function(obs){
+  temp<-dataset[dataset$appId==obs$appId,]
+  tempList<-c()
+  for(x in seq(1,nrow(temp))){
+    tempList<-c(tempList,stringdist(obs$revText,dataset[x,"revText"], method ="cosine"))
+  }
+  mean(tempList)
+}
+
+func_avg_levenshtein_dist_title<-function(obs){
+  temp<-dataset[dataset$appId==obs$appId,]
+  tempList<-c()
+  for(x in seq(1,nrow(temp))){
+    tempList<-c(tempList,stringdist(obs$revTitle,dataset[x,"revTitle"], method ="lv"))
+  }
+  mean(tempList)
+}
+
+func_avg_levenshtein_dist_text<-function(obs){
+  temp<-dataset[dataset$appId==obs$appId,]
+  tempList<-c()
+  for(x in seq(1,nrow(temp))){
+    tempList<-c(tempList,stringdist(obs$revText,dataset[x,"revText"], method ="lv"))
+  }
+  mean(tempList)
+}
+
+func_brand_names_in_title<-function(obs){
+  if(grep(tolower(obs$revTitle),pattern = tolower(obs$appTitle))==1){
+    1
+  }else{
+    0
+  }
+}
+
+func_brand_names_in_text<-function(obs){
+  if(grep(tolower(obs$revText),pattern = tolower(obs$appTitle))==1){
+    1
+  }else{
+    0
+  }
+}
 
 
 
